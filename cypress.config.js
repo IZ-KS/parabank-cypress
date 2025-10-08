dc0c37ede88263b651a368ef11a127c1128d7eb9
IZ-KS <kslim.isaac@gmail.com>
2025-10-08 23:10:18 +0800

    Update the workflow changes

--- cypress.config.js
+++ cypress.config.js
@@ -5,6 +5,9 @@ module.exports = defineConfig({
     // This tells Cypress NOT to look for a support file like
     // cypress/support/e2e.js before running your tests.
     supportFile: false, 
+    // FIX: Update the specPattern to include the nested 'parabank-cypress' folder
+    // This tells Cypress to look for files in 'parabank-cypress/e2e'
+    specPattern: 'parabank-cypress/e2e/**/*.cy.{js,jsx,ts,tsx}', 
     setupNodeEvents(on, config) {
       // implement node event listeners here
     },
