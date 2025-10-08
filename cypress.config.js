8618eb46c6a342155f64bdd2ac0cd805313abfd3
IZ-KS <kslim.isaac@gmail.com>
2025-10-08 23:01:22 +0800

    supportFile false added

--- cypress.config.js
+++ cypress.config.js
@@ -2,6 +2,9 @@ const { defineConfig } = require("cypress");
 
 module.exports = defineConfig({
   e2e: {
+    // This tells Cypress NOT to look for a support file like
+    // cypress/support/e2e.js before running your tests.
+    supportFile: false, 
     setupNodeEvents(on, config) {
       // implement node event listeners here
     },
